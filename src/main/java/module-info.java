bd20fb132276e054da0f093d47f584f33323eb62
KoiByteDev <KoiByteDev@gmail.com>
2024-06-29 12:14:32 -0500

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,6 @@
 module com.example.chatapp_java {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
     opens com.example.chatapp_java to javafx.fxml;
     exports com.example.chatapp_java;
