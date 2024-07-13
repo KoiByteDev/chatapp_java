127ede81e4e6beeec4953784a4ba5ac69b2dfa56
Shazzy004 <ealvare622@gmail.com>
2024-07-12 21:33:28 -0500

    Interfaz y Dimensiones en codigo

--- src/main/java/com/example/chatapp_java/App.java
+++ src/main/java/com/example/chatapp_java/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 360, 300);
         stage.setTitle("Chat Application");
         stage.setScene(scene);
         stage.show();
